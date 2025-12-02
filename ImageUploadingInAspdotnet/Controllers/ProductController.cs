d38a63e1bc1de6a909d2c1d7eb1699ba78ce31ed
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-12-02 15:22:45 +0500

    add temp data

--- ImageUploadingInAspdotnet/Controllers/ProductController.cs
+++ ImageUploadingInAspdotnet/Controllers/ProductController.cs
@@ -15,7 +15,7 @@ namespace ImageUploadingInAspdotnet.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            return View(context.Products.ToList());
         }
 
         public IActionResult AddProduct()
@@ -28,20 +28,39 @@ namespace ImageUploadingInAspdotnet.Controllers
             string fileName = "";
             if (obj.photo != null)
             {
-               string folder =  Path.Combine(env.WebRootPath,"images");
-                fileName = Guid.NewGuid().ToString() + "_" + obj.photo.FileName; // to make the img name unique...
-                string filePath = Path.Combine(folder, fileName);
-                obj.photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                Product p = new Product()
+                var ext = Path.GetExtension(obj.photo.FileName);
+                var size = obj.photo.Length;
+                if (!(ext == ".png" || ext == ".jpg" || ext == ".jpeg"))
                 {
-                    Name = obj.Name,
-                    Price = obj.Price,
-                    ImagePath = "images/" + fileName
-                };
-                context.Products.Add(p);
-                context.SaveChanges();
-                TempData["msg"] = "Product Added Successfully";
-                return RedirectToAction("Index");
+                    if (size <= 1000000)
+                    {
+
+
+                        string folder = Path.Combine(env.WebRootPath, "images");
+                        fileName = Guid.NewGuid().ToString() + "_" + obj.photo.FileName; // to make the img name unique...
+                        string filePath = Path.Combine(folder, fileName);
+                        obj.photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                        Product p = new Product()
+                        {
+                            Name = obj.Name,
+                            Price = obj.Price,
+                            ImagePath = "images/" + fileName
+                        };
+                        context.Products.Add(p);
+                        context.SaveChanges();
+                        TempData["msg"] = "Product Added Successfully";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        TempData["size"] = "Img size should be less than 1 Mb";
+
+                    }
+                }
+                else
+                {
+                    TempData["Ext_error"] = "only png , jpg, jpeg extension img are allowed";
+                }
             }
             return View();
         }
--- ImageUploadingInAspdotnet/Program.cs
+++ ImageUploadingInAspdotnet/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=AddProduct}/{id?}");
+    pattern: "{controller=Product}/{action=Index}/{id?}");
 
 app.Run();
