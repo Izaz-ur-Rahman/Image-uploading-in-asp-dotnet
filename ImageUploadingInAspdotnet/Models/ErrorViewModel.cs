52f784113ed624a2a3d52249b2a17f4f9d23227c
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-12-02 10:22:13 +0500

    create and upload entire project

---
+++ ImageUploadingInAspdotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageUploadingInAspdotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
