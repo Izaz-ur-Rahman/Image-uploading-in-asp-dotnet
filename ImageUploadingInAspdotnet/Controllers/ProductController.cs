8dfd6dad3ea332145a7203481166e244ab206867
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-12-02 12:04:40 +0500

    add view for add product view

---
+++ ImageUploadingInAspdotnet/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using ImageUploadingInAspdotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploadingInAspdotnet.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Product obj;
+        ImageDbContext context;
+        public ProductController(ImageDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
